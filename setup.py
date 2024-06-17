1c77d7cee54170de007acb9de45fb736b1f4b1f9
Sarthak Rastogi <61943500+sarthakrastogi@users.noreply.github.com>
2024-06-17 18:09:04 +0530

    downgrade np version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description=readme_content,
     long_description_content_type="text/markdown",
     install_requires=[
-        "numpy==1.26.0",
+        "numpy==1.24.0",
         "networkx==3.2.1",
         "nltk==3.8.1",
         "litellm==1.34.0",
